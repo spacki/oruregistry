de39371c5965748b41062d5ac0829bc9f5c26af5
Dmytro Rud <unixoid@web.de>
2011-01-13 16:15:16 +0100

    drop obsolete files

--- src/main/groovy/org/openehealth/ipf/ws/oru/SampleModelExtension.groovy
+++
@@ -1,14 +0,0 @@
-package org.openehealth.ipf.ws.oru
-
-import org.apache.camel.model.ProcessorDefinition
-class SampleModelExtension {
-
-     static extensions = {
-         
-         ProcessorDefinition.metaClass.reverse = {
-             delegate.transmogrify { it.reverse() }
-         }
-         
-     }
-     
-}
